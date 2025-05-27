e46e3dedb88587a8bea47cdea43e84bcd9e57511
ramirezplanet <ramirezmunoz@serviceplanet.nl>
2025-05-27 11:16:05 +0200

    cambios para produccion

--- src/auth/axiosConfig.js
+++ src/auth/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { checkToken } from "./AuthService"; // ajusta ruta según tu proyecto
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/api",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000/",
   withCredentials: true,
 });
 
